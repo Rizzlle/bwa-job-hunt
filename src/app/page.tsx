403bca716b81ce5aa293201e61069674083c3481
Hariyanto <hariyan242@gmail.com>
2023-08-19 13:29:12 +0700

    update: clients

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Clients from "@/components/organisms/Clients";
 import Hero from "@/components/organisms/Hero";
 
 export default function Home() {
 	return (
-		<div className="px-32">
+		<div className="px-32 mb-10">
 			<Hero />
+			<Clients />
 		</div>
 	);
 }
